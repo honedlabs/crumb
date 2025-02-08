7d01ea57c7043ae9356c430613b817307b49a5db
jdw5 <87954417+jdw5@users.noreply.github.com>
2025-02-08 01:18:55 +0000

    Fix styling

--- src/Attributes/Crumb.php
+++ src/Attributes/Crumb.php
@@ -9,7 +9,7 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class Crumb
 {
-    public function __construct(protected readonly string $crumb) 
+    public function __construct(protected readonly string $crumb)
     {
         //
     }
