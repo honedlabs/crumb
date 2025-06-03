d2adc15749a46364930f4e819c189bbf1e935c63
jdw5 <joshuadwallace1@gmail.com>
2025-06-03 02:01:00 +0000

    feat(abn): art

--- src/Middleware/ShareCrumb.php
+++ src/Middleware/ShareCrumb.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Honed\Crumb\Middleware;
 
+use Closure;
 use Honed\Crumb\Facades\Crumbs;
 
 class ShareCrumb
@@ -11,9 +12,9 @@ class ShareCrumb
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     * @param  string|null  $crumb
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param string|null $crumb
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle($request, $next, $crumb = null)
@@ -29,7 +30,7 @@ class ShareCrumb
 
     /**
      * Retrieve the default crumb to use if none is provided.
-     *
+     * 
      * @return string|null
      */
     protected function getDefaultCrumb()
